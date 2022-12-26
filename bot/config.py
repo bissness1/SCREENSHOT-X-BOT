f392dbe6fe3357b19b000913f8f3535e7bbbb83d
bissness1 <107295935+bissness1@users.noreply.github.com>
2022-12-26 15:15:21 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,15 +3,15 @@ from pathlib import Path
 
 class Config:
 
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = os.environ.get('API_HASH')
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
+    API_ID = int(os.environ.get('API_ID','6534707'))
+    API_HASH = os.environ.get('API_HASH','4bcc61d959a9f403b2f20149cbbe627a')
+    BOT_TOKEN = os.environ.get('BOT_TOKEN','5541380668:AAGH9yGr_4leFAgG35raDvAi1Q82ynMJGG0')
     SESSION_NAME = os.environ.get('SESSION_NAME', ':memory:')
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL'))
-    DATABASE_URL = os.environ.get('DATABASE_URL')
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL','-1001843564893'))
+    DATABASE_URL = os.environ.get('DATABASE_URL','mongodb+srv://Uploader:Uploader@cluster0.ba0ppxa.mongodb.net/?retryWrites=true&w=majority')
     AUTH_USERS = [int(i) for i in os.environ.get('AUTH_USERS', '').split(' ')]
-    MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 2))
-    MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 600))
+    MAX_PROCESSES_PER_USER = int(os.environ.get('MAX_PROCESSES_PER_USER', 3))
+    MAX_TRIM_DURATION = int(os.environ.get('MAX_TRIM_DURATION', 6000))
     TRACK_CHANNEL = int(os.environ.get('TRACK_CHANNEL', False))
     SLOW_SPEED_DELAY = int(os.environ.get('SLOW_SPEED_DELAY', 5))
     HOST = os.environ.get('HOST', '')
